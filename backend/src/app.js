a4aa7c9068b77a31e9fa5a2880ea651e77689e88
Rafael Ferreira <rafaelheferreira@gmail.com>
2023-12-13 01:08:25 -0300

    atualizações

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express()
+
+// habilita o cors
+app.use(cors());
+
+// indicar para o express ler body com json
+app.use(express.json());
 
 //mock
 const usuarios = [
@@ -20,20 +28,31 @@ const usuarios = [
 ]
 
 // funções auxiliares
-function buscarUsuariosPorId(username) {
-    return usuarios.filter(usuarios => usuarios.username == username)
+function buscarUsuariosPorUsername(username) {
+    return usuarios.filter(user => user.username == username)
 }
-
-app.get('/login', (req, res) => {
-    if (usuarios.username === 'rafael@finance.com') {
-        res.status(200).send('Deu certo')
-    }
-    res.status(404).send('Deu errado')
-})
 
 app.get('/login/:username', (req, res) => {
     //let index = req.params.id
-    res.json(buscarUsuariosPorId(req.params.username))
+    res.json(buscarUsuariosPorUsername(req.params.username))
 })
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    // logica de autenticação
+    const user = usuarios.find(user => user.username === username);
+    //const userPassword = usuarios.find(pw => pw.password === password);
+
+    if (user) {
+        if (user.password === password) {
+            res.json({ ok: true, message: 'Autenticação bem-sucedida' });
+        } else {
+            res.json({ ok: false, message: 'Senha incorreta' });
+        }
+    } else {
+        res.json({ ok: false, message: 'Username não encontrado' })
+    }
+});
 
 export default app
---
+++ frontend/login/login.js
@@ -0,0 +1,31 @@
+const formLogin = document.querySelector('#formLogin');
+
+formLogin.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+
+    try {
+        const resp = await fetch(`http://localhost:3005/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const user = await resp.json();
+        const errormsg = document.querySelector('.errormsg')
+
+        if (user.ok) {
+            errormsg.classList.add('hidden')
+        } else {
+            //alert(user.message)
+            errormsg.classList.remove('hidden')
+            errormsg.innerHTML = user.message
+        }
+    } catch (error) {
+        console.log('Erro na req:', error)
+    }
+});
